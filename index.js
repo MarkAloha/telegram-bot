d77e2ad3c4bb831ea90787a839a74eeec7c8a8e1
Mark Klimentov <klimentov51@mail.ru>
2025-07-12 02:19:36 +0300

    chore: поднятие версии dotenv

--- index.js
+++ index.js
@@ -2,31 +2,35 @@ require('dotenv').config();
 
 const { Bot, GrammyError, HttpError } = require('grammy');
 
-const bot = new Bot (process.env.BOT_API_KEY);
+const bot = new Bot(process.env.BOT_API_KEY);
 
 // обработчик событий
-bot.catch((err) => { const ctx = err.ctx;
+bot.catch((err) => {
+    const ctx = err.ctx;
+
     console.error(`Error while handling update ${ctx.update.update_id}:`); const e = err.error;
+
     if (e instanceof GrammyError) {
-     console.error('Error in request:', e.description);
-    } else if (e instanceof HttpError) { console.error('Could not contact Telegram:', e);
+        console.error('Error in request:', e.description);
+    } else if (e instanceof HttpError) {
+        console.error('Could not contact Telegram:', e);
     } else {
-    console.error('Unknown error:', e);
+        console.error('Unknown error:', e);
     }
-    });
+});
 
 bot.start();
 
 bot.hears(/нет/, async (ctx) => {
     await ctx.reply(
-     "пи.. что?? на нет и суда нет!",
+        "пи.. что?? на нет и суда нет!",
     );
-   });
+});
 
 bot.hears(/да/, async (ctx) => {
-await ctx.reply(
-"пи.. что?? дора-дура ответ!",
-);
+    await ctx.reply(
+        "пи.. что?? дора-дура ответ!",
+    );
 });
 
 bot.api.setMyCommands([
@@ -34,28 +38,28 @@ bot.api.setMyCommands([
     { command: 'say_hello', description: 'Получить приветствие' },
     { command: 'hello', description: 'Получить приветствие' },
     { command: 'give_sex', description: 'Получить пизды' },
-    ]);
+]);
 
 bot.command('start', async (ctx) => {
- await ctx.reply(
-  'Привет! Я - Бот 🤖',
- );
+    await ctx.reply(
+        'Привет! Я - Бот 🤖',
+    );
 });
 
 bot.command('give_sex', async (ctx) => {
     await ctx.reply(
-     `${ctx.from.first_name}, ты что охуел?`,
+        `${ctx.from.first_name}, ты что охуел?`,
     );
-   });
+});
 
 bot.command('help', async (ctx) => {
     await ctx.reply(
-     'не',
+        'не',
     );
-   });
-   
-bot.on("message", async (ctx) => {
- await ctx.reply('Не выдумывай');
+});
+
+bot.on('message', async (ctx) => {
+    await ctx.reply('Не выдумывай');
 });
 
 
